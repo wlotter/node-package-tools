e00db3f72452d286161ab837186e28ff7dfd1d17
William Lotter <lotter.wbs@gmail.com>
2019-01-29 23:14:06 +0000

    prototype configuration reading

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 #!/usr/bin/env node
 
+import {config, doesConfigExist} from './configuration/configuration-reader'
+import Logger from './logger';
+
+const logger = Logger();
+
+require('yargs') // eslint-disable-line
+  .command('config [option]', 'read and edit config', (yargs) => {
+    yargs
+      .positional('option', {
+        describe: 'config option to modify',
+        type: 'string'
+      })
+  }, (argv) => {
+		const option = argv.option;
+		if (!option) {
+			logger.error('an option needs to be supplied');
+			return;
+		}
+		if (!doesConfigExist(option)) {
+			logger.error('no config option ' + option);
+			return;
+		}
+		const configuration = config();
+		console.log(configuration[option])
+  })
+  .argv
