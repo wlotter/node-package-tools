a82a504c08752371e356f01116323aabbe80c9f7
Billy Lotter <lotter.wbs@gmail.com>
2019-02-02 18:44:25 +0000

    Add tar file extension warning

--- src/commands/tar/tar.js
+++ src/commands/tar/tar.js
@@ -3,12 +3,18 @@ import Tar from 'tar';
 import * as ConfigIO from '../../configuration/configuration-io';
 import Logger from '../../logger';
 
+const TAR_REGEX = /^.*\.(tgz|tar\.gz)$/
+
 export default function tar(argv) {
 	const config = ConfigIO.getConfig();
 
 	const {name, src} = argv;
 	const tarName = name ? name : resolveTarNameFromConfig(config);
 	const tarSrc = src ? src : config['tar/src'];
+
+	if (!TAR_REGEX.test(tarName)) {
+		Logger.warn(tarName + ' does not have an appropriate file extension!')
+	}
 
 	Tar.create({
 		gzip: true,
