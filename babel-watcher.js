ab08fee221ae149d2af7cd1c9139cbda2a3952e0
wlotter <lotter.wbs@gmail.com>
2019-02-03 19:05:20 +0000

    add linting to watcher script

--- babel-watcher.js
+++ babel-watcher.js
@@ -1,67 +1,69 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-console */
+
 // Dev script - I had permission issues when using babels watch option
 
 const Chokidar = require('chokidar');
 const spawn = require('child_process').spawn;
 const fs = require('fs');
 
-const loud = process.env.SILENT ? false : true;
+const loud = process.env.SILENT ? false : true; // eslint-disable-line
 const writeStdout = (message) => {
-	loud && process.stdout.write(message);
-}
+  loud && process.stdout.write(message); // eslint-disable-line
+};
 
 writeStdout('Babel watcher getting ready...\n');
 var watcher = Chokidar.watch('src', {persistent: true});
 
 // hack to stop an initial onslaught of compiles
 setTimeout(() => {
-	watcher
-		.on('add', onAdd)
-		.on('change', onChange)
-		.on('unlink', onUnlink)
-		.on('error', onError)
+  watcher
+    .on('add', onAdd)
+    .on('change', onChange)
+    .on('unlink', onUnlink)
+    .on('error', onError);
 
-	writeStdout('Ready to watch!\nPerforming initial compile...\n');
-	babelRecompileAndChmod()
+  writeStdout('Ready to watch!\nPerforming initial compile...\n');
+  babelRecompileAndChmod();
 }, 500);
 	
 function onAdd() {
-	babelRecompileAndChmod();
+  babelRecompileAndChmod();
 }
 
 function onChange() {
-	babelRecompileAndChmod();
+  babelRecompileAndChmod();
 }
 
 
 function onUnlink() {
-	babelRecompileAndChmod();
+  babelRecompileAndChmod();
 }
 
 function onError(error) {
-	console.error('Error happened', error)
+  console.error('Error happened', error);
 }
 
 function babelRecompileAndChmod() {
-	const babel = spawn('./node_modules/.bin/babel', [
-		'src',
-		'--out-dir',
-		'target'
-	]);
+  const babel = spawn('./node_modules/.bin/babel', [
+    'src',
+    '--out-dir',
+    'target'
+  ]);
 	
-	babel.stdout.on('data', function (data) {
-		writeStdout(data);
-	});
+  babel.stdout.on('data', function (data) {
+    writeStdout(data);
+  });
 	
-	babel.stderr.on('data', function (data) {
-		writeStdout(data);
-	});
+  babel.stderr.on('data', function (data) {
+    writeStdout(data);
+  });
 	
-	babel.on('exit', function (data) {
-		if (data) writeStdout('Exit data: ' + data + '\n');
-		fs.chmod('target/index.js', 0o711, err => {
-			if (err) console.log(err);
-		});
-	});
+  babel.on('exit', function (data) {
+    if (data) writeStdout('Exit data: ' + data + '\n');
+    fs.chmod('target/index.js', 0o711, err => {
+      if (err) console.log(err);
+    });
+  });
 }
